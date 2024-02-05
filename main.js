1d944bf1d6c26ec832e6a3be56e63ed5e51c50c7
maiigl <michael.golm96@googlemail.com>
2024-02-05 16:22:49 +0100

    html code gesäubert

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-
-    
+// script.js
 
 const random = () => {
-    const arr1 = ['Öffis nehmen', 'vegan essen', 'Klimademo besuchen', 'deinem Abgeordneten schreiben', 'Licht sparen']
-    const arr2 = ['Jane Goodall', 'Al Gore', 'Greta Thunberg', 'Ban Ki Moon', 'Barack Obama']
-    const arr3 = ['dich', 'deine Kinder', 'die Natur', 'nachfolgende Generationen', 'deinen Nachbarn']
+    const arr1 = ['Öffis nehmen', 'vegan essen', 'Klimademo besuchen', 'deinem Abgeordneten schreiben', 'Licht sparen'];
+    const arr2 = ['Jane Goodall', 'Al Gore', 'Greta Thunberg', 'Ban Ki Moon', 'Barack Obama'];
+    const arr3 = ['dich', 'deine Kinder', 'die Natur', 'nachfolgende Generationen', 'deinen Nachbarn'];
     const result = 
-    `Deine heutige klimaschonende Maßnahme ist: 
-    "${arr1[Math.floor(Math.random()*arr1.length)]}". 
-    Die inspiration dazu kommt von:
-    ${arr2[Math.floor(Math.random()*arr2.length)]}
-    und du tust es für: 
-    ${arr3[Math.floor(Math.random()*arr1.length)]}`;
-}
-
+      `Deine heutige klimaschonende Maßnahme ist: 
+      "${arr1[Math.floor(Math.random()*arr1.length)]}". 
+      Die Inspiration dazu kommt von:
+      ${arr2[Math.floor(Math.random()*arr2.length)]}
+      und du tust es für: 
+      ${arr3[Math.floor(Math.random()*arr1.length)]}`;
+    return result;
+  };
+  
+  document.getElementById('showResult').addEventListener('click', function() {
+    document.getElementById('result').innerHTML = random();
+  });
+  
